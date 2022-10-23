7057a267857e37f835462793ec84b5574420bc55
Der-Alex <alexander.thimm@googlemail.com>
2022-10-23 13:15:50 +0200

    added some scripts

--- scripts.js
+++ scripts.js
@@ -1,2 +1,17 @@
 // create new Worker
 require('creepFactory').createWorker(Game.spawns.Spawn1);
+require('creepFactory').createBuilder(Game.spawns.Spawn1);
+require('creepFactory').createHarvester(Game.spawns.Spawn1);
+require('creepFactory').createUpgrader(Game.spawns.Spawn1);
+
+// create new harvester with structure priority
+require('creepFactory').createHarvester(Game.spawns.Spawn1, STRUCTURE_EXTENSION); // STRUCTURE_SPAWN, STRUCTURE_TOWER
+
+// change role
+Game.creeps[Object.keys(Game.creeps).find((creep) => Game.creeps[creep].memory.role == require('role').HARVESTER)].memory.role = require('role').BUILDER;
+Game.creeps[Object.keys(Game.creeps).find((creep) => Game.creeps[creep].memory.role == require('role').HARVESTER)].memory.role = require('role').BUILDER;
+
+// find by role
+Object.keys(Game.creeps).filter((creep) => Game.creeps[creep].memory.role == require('role').HARVESTER).length;
+Object.keys(Game.creeps).filter((creep) => Game.creeps[creep].memory.role == require('role').BUILDER).length;
+Object.keys(Game.creeps).filter((creep) => Game.creeps[creep].memory.role == require('role').UPGRADER).length;
